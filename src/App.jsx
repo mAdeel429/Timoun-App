b7b9ab90435808ea2bbc38a1cad965c94323a931
mAdeel429 <muadeel752@gmail.com>
2026-01-11 14:05:27 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,61 @@
-// import './App.css';
+// // import './App.css';
 
-// function App() {
+// // function App() {
+// //   return (
+// //    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+// //       <h1 className="text-4xl font-bold text-white">
+// //         Tailwind CSS Working 🚀
+// //       </h1>
+// //     </div>
+// //   );
+// // }
+
+// // export default App;
+
+// // import { useState } from "react";
+// // import LoginScreen from "./screens/LoginScreen";
+// // import MenuScreen from "./screens/MenuScreen";
+// // import WatchScreen from "./screens/WatchScreen";
+// // import PlayerScreen from "./screens/PlayerScreen";
+// // import TunesScreen from "./screens/TunesScreen";
+// // import "./App.css";
+
+// export default function App() {
+//   // const [currentScreen, setCurrentScreen] = useState("login-screen");
+
+//   // const showScreen = (screenId) => {
+//   //   setCurrentScreen(screenId);
+//   // };
+
 //   return (
-//    <div className="min-h-screen flex items-center justify-center bg-gray-900">
-//       <h1 className="text-4xl font-bold text-white">
-//         Tailwind CSS Working 🚀
-//       </h1>
-//     </div>
+// //     <div className="app-root gradient-warm safe-area">
+// //   <div className={`screen ${currentScreen === "login-screen" ? "" : "screen-hidden"}`}>
+// //     <LoginScreen showScreen={showScreen} />
+// //   </div>
+// //   <div className={`screen ${currentScreen === "menu-screen" ? "" : "screen-hidden"}`}>
+// //     <MenuScreen showScreen={showScreen} />
+// //   </div>
+// //   <div className={`screen ${currentScreen === "watch-screen" ? "" : "screen-hidden"}`}>
+// //     <WatchScreen showScreen={showScreen} />
+// //   </div>
+// //   <div className={`screen ${currentScreen === "player-screen" ? "" : "screen-hidden"}`}>
+// //     <PlayerScreen showScreen={showScreen} />
+// //   </div>
+// //   <div className={`screen ${currentScreen === "tunes-screen" ? "" : "screen-hidden"}`}>
+// //     <TunesScreen showScreen={showScreen} />
+// //   </div>
+// // </div>
+// <></>
 //   );
 // }
 
-// export default App;
-
-// import { useState } from "react";
-// import LoginScreen from "./screens/LoginScreen";
-// import MenuScreen from "./screens/MenuScreen";
-// import WatchScreen from "./screens/WatchScreen";
-// import PlayerScreen from "./screens/PlayerScreen";
-// import TunesScreen from "./screens/TunesScreen";
-// import "./App.css";
+import React from 'react'
+import video from './assets/1.mp4'
 
 export default function App() {
-  // const [currentScreen, setCurrentScreen] = useState("login-screen");
-
-  // const showScreen = (screenId) => {
-  //   setCurrentScreen(screenId);
-  // };
-
   return (
-//     <div className="app-root gradient-warm safe-area">
-//   <div className={`screen ${currentScreen === "login-screen" ? "" : "screen-hidden"}`}>
-//     <LoginScreen showScreen={showScreen} />
-//   </div>
-//   <div className={`screen ${currentScreen === "menu-screen" ? "" : "screen-hidden"}`}>
-//     <MenuScreen showScreen={showScreen} />
-//   </div>
-//   <div className={`screen ${currentScreen === "watch-screen" ? "" : "screen-hidden"}`}>
-//     <WatchScreen showScreen={showScreen} />
-//   </div>
-//   <div className={`screen ${currentScreen === "player-screen" ? "" : "screen-hidden"}`}>
-//     <PlayerScreen showScreen={showScreen} />
-//   </div>
-//   <div className={`screen ${currentScreen === "tunes-screen" ? "" : "screen-hidden"}`}>
-//     <TunesScreen showScreen={showScreen} />
-//   </div>
-// </div>
-<></>
-  );
+    <div>
+      <video src={video} controls />
+    </div>
+  )
 }
-
